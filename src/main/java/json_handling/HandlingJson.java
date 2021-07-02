fcae1ce375d41969ca07f2efb5c844ec0681b7f7
IgorDems <dems_i@yahoo.com>
2021-07-02 03:36:03 -0500

    assasment api done

---
+++ src/main/java/json_handling/HandlingJson.java
@@ -0,0 +1,53 @@
+package json_handling;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+
+
+
+public class HandlingJson {
+	
+	public JsonObject getRequest(HttpGet getRequest) throws IOException {
+
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        getRequest.addHeader("accept", "application/json");
+        HttpResponse response = httpClient.execute(getRequest);
+
+        if (response.getStatusLine().getStatusCode() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : "
+                    + response.getStatusLine().getStatusCode());
+        }
+
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader((response.getEntity().getContent())));
+
+        String line;
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+
+        JsonObject jsonObject = deserialize(stringBuilder.toString());
+        bufferedReader.close();
+
+        return jsonObject;
+    }
+
+    public JsonObject deserialize(String json) {
+        Gson gson = new Gson();
+        JsonObject jsonClass = gson.fromJson(json, JsonObject.class);
+        return jsonClass;
+    }
+
+}
--- src/test/java/REST_API/TestApi.java
+++ src/test/java/REST_API/TestApi.java
@@ -2,19 +2,28 @@ package REST_API;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import json_handling.HandlingJson;
+
+import com.google.gson.JsonObject;
+import org.apache.http.client.methods.HttpGet;
 import org.testng.Assert;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestApi {
+	HandlingJson hj = new HandlingJson();
 
     String url = "https://swapi.dev/api/";
     String endpoint = "people/";
 
-    @Test
+    @Test(groups = { "test1" })
     public void successfulResponse() {
         System.out.println("Test1");
         Response response = RestAssured.get(url + endpoint);
@@ -25,7 +34,7 @@ public class TestApi {
     }
 
 
-    @Test
+    @Test(groups = { "test2" })
     public void peopleNameCount() {
  
         System.out.println("Test2");
@@ -46,23 +55,49 @@ public class TestApi {
 
     }
 
-    @Test
-    public void peopleHeight() {
-        System.out.println("Test3");
-        System.out.println("Verify that the total number of people where height is greater than\n" +
-                "200 matches the expected count (10 at the time this was assigned)");
-//        given().get("https://swapi.dev/api/people/").then().statusCode(200).body("count",>200);
-//        given().get("https://swapi.dev/api/people/").then().statusCode(200).body("results.height");
+    @Test(groups = { "test-group" })
 
-    }
+    public void GetHeight() throws IOException {
+        String url = "https://swapi.dev/api/";
+        String endpoint = "people/";
+        String[] expected = {"Darth Vader", "Chewbacca", "Roos Tarpals", "Rugor Nass", "Yarael Poof", "Lama Su", "Taun We", "Grievous", "Tarfful", "Tion Medon"};
+        String[] result = new String[10];
+        int i = 1;
+        int count = 0;
+        while (i < 84) {
 
-    @Test
-    public void tenIndividuals() {
-        System.out.println("Test4");
+            HttpGet person = new HttpGet(url+endpoint+String.valueOf(i)+"/");
+            JsonObject personJSON = new JsonObject();
+            personJSON = hj.getRequest(person);
+
+
+            if (RestAssured.get(url + endpoint + "/" + String.valueOf(i)).getStatusCode() == 200) {
+                if ( personJSON.get("height").getAsInt()>200 ){
+                    System.out.println("height = " + personJSON.get("height").getAsInt() );
+                    result[count] = personJSON.get("name").getAsString();
+                    count++;
+                }
+                System.out.println("count = " + count );
+                System.out.println("i = " + i );
+            }
+            i++;
+            if(i==17||i==29){
+                i++;
+            }
+        }
+
+        System.out.println("<== The total number of people where \n" +
+                "height is greater than 200 is: "+count);
+        assertTrue(count>=10);
+
         System.out.println("Verify that the 10 individuals returned are:\n" +
-                "Darth Vader. Chewbacca, Roos Tarpals, Rugor Nass, Yarael Poof, Lama Su, Tuan Wu, Grievous, Tarfful, Tion Medon");
-    }
+                "Darth Vader, Chewbacca, Roos Tarpals, Rugor Nass, Yarael Poof, Lama Su, Tuan Wu, Grievous, Tarfful, Tion Medon");
+        for (String a: result) {
+            System.out.println("result array = " + a+", ");
+        }
 
+        assertArrayEquals(expected,result);
+    }
 
 }
 
